1e8d71971114fb17b1e30befd99b0db851c4107c
YuIzawa <yuizalp@gmail.com>
2016-07-24 19:30:24 +0900

    キャプチャをクロールするスクリプトを追加

---
+++ scripts/scraping_image.rb
@@ -0,0 +1,20 @@
+require 'open-uri'
+require 'nokogiri'
+
+# newgame 3話
+# url = 'http://blog.livedoor.jp/anime_info555/archives/1059432369.html'
+# newgame 2話
+# url = 'http://blog.livedoor.jp/anime_info555/archives/1059231869.html'
+# newgame 1話
+url = 'http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
+charset = nil
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.css('img').each do |image|
+  p image.attribute('src').value
+end
