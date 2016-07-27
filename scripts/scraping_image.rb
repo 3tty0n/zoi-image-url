614cb3cbc71c55aca2af47daf293595aca3d9bdd
YuIzawa <yuizalp@gmail.com>
2016-07-27 16:10:00 +0900

    newgame! 4話のキャプチャを追加

--- scripts/scraping_image.rb
+++ scripts/scraping_image.rb
@@ -1,12 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
 
+# newgame 4話
+url = 'http://blog.livedoor.jp/anime_info555/archives/1059638437.html'
 # newgame 3話
 # url = 'http://blog.livedoor.jp/anime_info555/archives/1059432369.html'
 # newgame 2話
 # url = 'http://blog.livedoor.jp/anime_info555/archives/1059231869.html'
 # newgame 1話
-url = 'http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
+# url = 'http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
 charset = nil
 html = open(url) do |f|
   charset = f.charset
