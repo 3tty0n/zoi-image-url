6197e4ace2bfbd44b01cb2cf4ac08e27440faaca
YuIzawa <yuizalp@gmail.com>
2016-07-28 16:24:08 +0900

    スクリプトの修正

--- get_image_url.sh
+++ get_image_url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ruby scripts/scraping_image.rb >> image_url/tmp.txt
-sed "s/$/,/g" image_url/tmp.txt >> image_url/$1.txt
+
+ruby scripts/scraping_image.rb > image_url/tmp.txt
+sed "s/$/,/g" image_url/tmp.txt > image_url/$1.txt
 rm image_url/tmp.txt
 sed -e '$d' image_url/$1.txt
-
