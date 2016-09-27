32a153090d5e2eaff363f37bf7c074ca76dc3f66
YuIzawa <yuizalp@gmail.com>
2016-09-27 17:09:13 +0900

    newgame! 12話(最終回)のキャプチャを追加

--- scripts/scraping_image.rb
+++ scripts/scraping_image.rb
@@ -1,7 +1,8 @@
 require 'open-uri'
 require 'nokogiri'
 
-newgame_urls = ['http://blog.livedoor.jp/anime_info555/archives/1059025536.html',
+newgame_urls = [
+  'http://blog.livedoor.jp/anime_info555/archives/1059025536.html',
   'http://blog.livedoor.jp/anime_info555/archives/1059231869.html',
   'http://blog.livedoor.jp/anime_info555/archives/1059432369.html',
   'http://blog.livedoor.jp/anime_info555/archives/1059638437.html',
@@ -11,9 +12,10 @@ newgame_urls = ['http://blog.livedoor.jp/anime_info555/archives/1059025536.html'
   'http://blog.livedoor.jp/anime_info555/archives/1060438150.html',
   'http://blog.livedoor.jp/anime_info555/archives/1060634432.html',
   'http://blog.livedoor.jp/anime_info555/archives/1060822047.html',
-  'http://blog.livedoor.jp/anime_info555/archives/1061001328.html'
+  'http://blog.livedoor.jp/anime_info555/archives/1061001328.html',
+  'http://blog.livedoor.jp/anime_info555/archives/1061174569.html'
 ]
-episode = 11
+episode = 12
 url = newgame_urls[episode - 1]
 charset = nil
 html = open(url) do |f|
